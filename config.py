4a913b5e5ee24a19f2f236bc6d5a8fd8323c801b
CeliaMarion <35692894+CeliaMarion@users.noreply.github.com>
2018-02-25 08:58:24 +0100

    messages for default paths corrected

---
+++ config.py
@@ -0,0 +1,49 @@
+# coding: utf-8 
+
+import os
+import argparse
+
+folderMain = os.getcwd() 
+print("Chemin d'accès à votre dossier de travail :")
+print("Path of working folder :")
+print(folderMain,"\n")
+
+folderLexicon = os.path.join(folderMain,"lexicons") 
+print("Chemin attendu pour le dossier 'lexicons' :")
+print("Expected path for 'lexicons' folder :")
+print(folderLexicon,"\n") 
+
+folderPhonemes = os.path.join(folderMain,"phonemes")
+print("Chemin pour le dossier 'phonemes' qui est généré automatiquement :")
+print("Path for the auto-generated folder 'phonemes' :")
+print(folderPhonemes,"\n") 
+
+folderSVG = os.path.join(folderMain,"svg")
+print("Chemin attendu pour le dossier 'svg' :")
+print("Expected path for 'svg' folder :")
+print(folderSVG,"\n")
+
+folderTemplate = os.path.join(folderSVG,"svg_template")
+print("Chemin attendu pour le dossier 'svg_template' :")
+print("Expected path for 'svg_template' folder :")
+print(folderSVG,"\n")
+
+folderInter = os.path.join(folderSVG,"svg_inter")
+print("Chemin attendu pour le dossier 'svg_inter' :")
+print("Expected path for 'svg_inter' folder :")
+print(folderSVG,"\n")
+
+folderFinaux = os.path.join(folderSVG,"svg_finaux")
+print("Chemin attendu pour le dossier 'svg_finaux' :")
+print("Expected path for 'svg_finaux' folder :")
+print(folderSVG,"\n")
+
+folderClean = os.path.join(folderSVG,"svg_clean")
+print("Chemin attendu pour le dossier 'svg_clean' :")
+print("Expected path for 'svg_clean' folder :")
+print(folderSVG,"\n")
+
+folderInterface = os.path.join(folderMain,"interface")
+print("Chemin attendu pour le dossier 'interface' :")
+print("Expected path for 'interface' folder :")
+print(folderSVG,"\n")
