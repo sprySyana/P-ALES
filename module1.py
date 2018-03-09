d923d4e4938e1baf2fc29329df3b3dae6c47ac5f
AnaysM <33751081+sprySyana@users.noreply.github.com>
2018-03-09 08:50:07 +0000

    add english comments

--- module1.py
+++ module1.py
@@ -13,49 +13,62 @@ def extractPairs(folderLexicon,folderPhonemes,folderMain):
     """Extract pairs of phonemes and their frequencies for each phonetic lexicon."""
     print ("Extraction  des paires de phonèmes adjacents.")
     # Dictionnaire qui stocke chaque paire de phonèmes et ses fréquences
+    #   Dictionary to stock each phonem paire(Key) with its frequency(Value)
     dicPairs = {}
     # Liste qui stocke les phonèmes rencontrés pour chaque langue
+    #   List of phonems for the currently treated langue
     listP = []
     # Compteur du nombre de paires recensées
+    #   Counter of (different) pairs met 
     # ~ nb_occ = 0
     # Création du dossier de sortie s'il n'existe pas
+    #   Create folder if not existing or path not specified
     if not os.path.isdir(folderPhonemes):
         os.mkdir(folderPhonemes)
     try:
         # On obtient la liste des fichiers du répertoire
+        #   Retrieve list of folder in directory
         files = os.listdir(folderLexicon)
     except OSError as e:
         ("err n°{} lors de la lecture du dossier".format(e.errno))
         return False
     
     # On ouvre les lexiques un à un
+    #   Open lexicon files one by one
     for f in files:
         try:
             lexPho = open(os.path.join(folderLexicon,f),"r",encoding="utf8")
             # On récupère la langue dans le nom des fichiers
+            #   Retrieve langue in file name
             m = re.search(r"(.*)([_].*)",f)
             language = m.group(1)
             # Parcours du lexique phonétisé
+            #   Scan phonetised lexicon
             for line in lexPho:
                 phono = line.split(" ")
                 # ~ print (phono)
                 for i in range(0,len(phono)-2):
                     if (phono[i] != "\n"):
-                        # élément courant : current
+                        # élément courant : current element
                         courant = phono[i]
-                        # élément suivant : next
+                        # élément suivant : next element
                         suivant = phono[i+1]
                         # On forme les paires
+                        #   We forme the pairs
                         # ~ print ("paire : "+courant+" et "+suivant)
                         paire = courant+"\t"+suivant
                         phoneme = courant
                         # Ajout de la paire au dictionnaire
+                        #   Adding the pair in dicPairs
                         # Si la paire ne figure pas encore dans le dictionnaire
+                        #   If the pair has not been uncountered yet
                         if (paire not in dicPairs.keys()):
                             # Ajout de la paire dans en clé + fréquence=1
+                            #   Add new pair as key with freq=1
                             dicPairs[paire] = 1
                         else:
                             # Incrémentation de la fréquence
+                            #   Increment frequency
                             dicPairs[paire] += 1
                         if courant not in listP:
                             listP.append(courant)
@@ -63,8 +76,10 @@ def extractPairs(folderLexicon,folderPhonemes,folderMain):
         except IOError as e:
             print ("err n°{} lors de la lecture".format(e.errno))
         # On stocke la liste des phonèmes dans un fichier spécifique
+        #   Write the phonem list in the appropriate file
         try: 
-            # Création du fichier contenant tous les phonèmes 
+            # Création du fichier contenant tous les phonèmes
+            #   Creation of file containing all phonems
             outputName = os.path.join(folderPhonemes,language) + "_phonemes.txt"
             filePhonemes = open(outputName,mode="w",encoding="utf8") 
             for phoneme in listP: 
@@ -75,9 +90,10 @@ def extractPairs(folderLexicon,folderPhonemes,folderMain):
         del(listP[:])
     try:
         # Création du fichier d'écriture de sortie
+        #   Create the output writing file
         filePairs = open("pairesAdj.txt","w",encoding="utf8")
         # Tri du dictionnaire contenant toutes les paires par value (fréquence)
-        # (reverse pour tri décroissant)
+        #   Sorting of Dic by frequency value
         dicPairsO = OrderedDict(sorted(dicPairs.items(),
             key=lambda t:t[1],reverse=True))
         for key,value in dicPairsO.items():
